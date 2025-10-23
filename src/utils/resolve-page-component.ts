ac2d66921c5b00bfd47abfb37b8e5b8b34f907ff
Albert Akrong <alphaxsalt@gmail.com>
2025-10-23 08:28:35 +0100

    refactor orm em

--- src/utils/resolve-page-component.ts
+++
@@ -1,8 +0,0 @@
-export function resolvePageComponent(name: string, pages: Record<string, any>) {
-  for (const path in pages) {
-    if (path.includes(`/${name}.tsx`)) {
-      return pages[path]()
-    }
-  }
-  throw new Error(`Page not found: ${name}. Available pages: ${Object.keys(pages).join(', ')}`)
-}
