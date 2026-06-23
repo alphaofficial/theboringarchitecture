354e3edcaf6af482ace2df928c1747926dddd12e
Albert Akrong <alphaxsalt@gmail.com>
2026-06-23 18:43:11 +0100

    feat: remove legacy req deps

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,14 +1,9 @@
-import { MikroORM } from '@mikro-orm/core';
 import { User } from '@/models/User';
-import { PinoLogger } from '@/logger/pinoLogger';
 import { AppContext } from '@/runtime/context';
 
 declare module 'express-serve-static-core' {
 	interface Request {
 		ctx: AppContext;
-		orm: MikroORM;
-		database: MikroORM['em'];
-		logger: typeof PinoLogger;
 		user(): Promise<User | null>;
 		user_id(): User['id'] | null;
 		is_authenticated(): boolean;
