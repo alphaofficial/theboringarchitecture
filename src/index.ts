2e60ab18b01b9d3442876c31b45e28f68c11ce98
Albert Akrong <alphaxsalt@gmail.com>
2026-06-23 20:50:38 +0100

    feat: add comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'dotenv-defaults/config';
-import { PinoLogger } from '@/logger/pinoLogger';
 import variables from '@/config/variables';
 import { Bus } from '@/primitives/bus';
 import { shutdown } from '@/primitives/shutdown';
@@ -7,38 +6,24 @@ import { createApp } from '@/router/app';
 const port = variables.PORT;
 
 async function bootstrap() {
-  const { app, orm } = await createApp();
-  Bus.start();
+  const { app, ctx } = await createApp();
 
+  /** Starts the server */
   const server = app.listen(port, () => {
-    PinoLogger.info({ scope: 'bootstrap', message: 'Server running', url: `http://localhost:${port}`, port });
+    ctx.logger.info({ scope: 'bootstrap', message: 'Server running', url: `http://localhost:${port}`, port });
   });
 
-  const disposables = [{
-    async stop() {
-      if (server.listening) {
-        await new Promise<void>((resolve, reject) => {
-          server.close(err => {
-            if (err) {
-              reject(err);
-              return;
-            }
-            resolve();
-          });
-        });
-      }
+  /** Starts the event bus */
+  Bus.start();
 
-      await orm.close(true);
-    },
-  }];
+  /** Defines the cleanup functions for the application */
+  const disposables = [
+    { async stop() { server.close(); } },
+    { async stop() { await ctx.db.getConnection().close(true)}}
+  ];
 
   process.on('SIGTERM', () => void shutdown('SIGTERM', disposables));
   process.on('SIGINT', () => void shutdown('SIGINT', disposables));
 }
 
-const onBootstrapError = (err: unknown): void => {
-  PinoLogger.error({ scope: 'onBootstrapError', message: 'Failed to start server', err });
-  process.exit(1);
-};
-
-bootstrap().catch(onBootstrapError);
+bootstrap().catch(process.exit(1));
